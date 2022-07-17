a18b5b6b81f35c7a803a0c0d255573e267ffb62d
Roha <roohaejaz69@gmail.com>
2022-07-17 18:53:07 +0300

    Updates

---
+++ precache-manifest.6cc0332849a78bedc5835d0572fb9d17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f5351657e94cf2a415a",
+    "url": "/TextUtils-React/static/css/main.845a0cfa.chunk.css"
+  },
+  {
+    "revision": "9f5351657e94cf2a415a",
+    "url": "/TextUtils-React/static/js/main.9f535165.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "54d4ef26c5198954da45",
+    "url": "/TextUtils-React/static/js/runtime~main.54d4ef26.js"
+  },
+  {
+    "revision": "dca6849a3ff870d34e2841629b98eb9d",
+    "url": "/TextUtils-React/index.html"
+  }
+];
